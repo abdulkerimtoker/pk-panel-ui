63195ec338a5b020873b9212de5578595298af9f
Abdulkerim Toker <abdulkerim.toker@sekomyazilim.com.tr>
2019-06-27 18:12:28 +0300

    Player list

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -17,7 +17,10 @@ export default class PlayerList extends React.Component {
                     <Column title="ID" dataIndex="id" key="id"/>
                     <Column title="Name" dataIndex="name" key="name"/>
                     <Column title="GUID" dataIndex="uniqueId" key="uniqueId"/>
+                    <Column title="Faction" dataIndex="faction.name" key="faction.name"/>
                     <Column title="Troop" dataIndex="troop.name" key="troop.name"/>
+                    <Column title="Gold" dataIndex="gold" key="gold"/>
+                    <Column render={() => <Button>Manage</Button>} />
                 </Table>
             </div>
         );
