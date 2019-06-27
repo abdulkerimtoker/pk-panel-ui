2c390dbb4038bf34d8125a57e1bfb117e60b38b5
Abdulkerim Toker <abdulkerim.toker@sekomyazilim.com.tr>
2019-06-27 18:06:04 +0300

    Player list

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -1,5 +1,6 @@
 import React from "react";
-import {Button} from "antd";
+import {Button, Table} from "antd";
+import Column from "antd/es/table/Column";
 
 export default class PlayerList extends React.Component {
     constructor(props) {
@@ -9,14 +10,15 @@ export default class PlayerList extends React.Component {
     render() {
         return (
             <div>
-                <Button onClick={this.props.fetchPlayerList}>Fetch</Button>
-                {
-                    this.props.playerList ?
-                        this.props.playerList.map(player => (
-                            <h6>{player.name}</h6>
-                        ))
-                        : null
-                }
+                <div>
+                    <Button onClick={this.props.fetchPlayerList}>Fetch</Button>
+                </div>
+                <Table dataSource={this.props.playerList} rowKey="id">
+                    <Column title="ID" dataIndex="id" key="id"/>
+                    <Column title="Name" dataIndex="name" key="name"/>
+                    <Column title="GUID" dataIndex="uniqueId" key="uniqueId"/>
+                    <Column title="Troop" dataIndex="troop.name" key="troop.name"/>
+                </Table>
             </div>
         );
     }
