7e5ec846ad7fdbab191691ef974b5db0478ab3b4
Abdulkerim Toker <abdulkerim.toker@sekomyazilim.com.tr>
2019-08-06 15:11:27 +0300

    Decompose PlayerField

--- src/components/PlayerField.js
+++ src/components/PlayerField.js
@@ -20,143 +20,26 @@ export default class PlayerField extends React.Component {
         this.state = {};
     }
 
-    handleChange = (event) => {
-        let object = Object.assign({}, this.props.object);
-        switch (this.props.type) {
-            case fieldTypes.text:
-                object[this.props.field] = event.target.value;
-                break;
-            case fieldTypes.number:
-                object[this.props.field] = parseInt(event.target.value);
-                break;
-            case fieldTypes.troop:
-                let selectedTroop = this.props.objectList.filter((troop) => troop.id.toString() === event.toString())[0];
-                object[this.props.field] = selectedTroop;
-                break;
-            case fieldTypes.item:
-                let selectedItem = this.props.objectList.filter((troop) => troop.id.toString() === event.toString())[0];
-                object[this.props.field] = selectedItem;
-                this.setState({filteredItems: null});
-                break;
-            case fieldTypes.doorKey:
-                let selectedDoor = this.props.objectList.filter((door) => door.id.toString() === event.toString())[0];
-                object[this.props.field] = selectedDoor;
-                this.setState({filteredDoors: null});
-                break;
-        }
-        this.props.onChange(object);
-    };
-
-    onItemSearch = (searchTerm) => {
-        if (searchTerm && searchTerm !== '') {
-            let filteredItems = this.props.objectList.filter(item =>
-                item.id.toString().includes(searchTerm) || item.name.toLowerCase().includes(searchTerm.toLowerCase())
-            );
-            this.setState({filteredItems: filteredItems});
-        } else {
-            this.setState({filteredItems: null});
-        }
-    };
-
-    onDoorSearch = (searchTerm) => {
-        if (searchTerm && searchTerm !== '') {
-            let filteredDoors = this.props.objectList.filter(door =>
-                door.id.toString().includes(searchTerm) || door.name.toLowerCase().includes(searchTerm.toLowerCase())
-            );
-            this.setState({filteredDoors: filteredDoors});
-        } else {
-            this.setState({filteredDoors: null});
-        }
-    };
-
-    onBoardSearch = (searchTerm) => {
-        if (searchTerm && searchTerm !== '') {
-            let filteredBoards = this.props.objectList.filter(board =>
-                board.id.toString().includes(searchTerm) || board.name.toLowerCase().includes(searchTerm.toLowerCase())
-            );
-            this.setState({filteredBoards: filteredBoards});
-        } else {
-            this.setState({filteredBoards: null});
-        }
-    };
-
     render() {
         let inputField;
 
         switch (this.props.type) {
             case fieldTypes.text:
-                inputField = <Input type="text" value={this.props.object[this.props.field]} onChange={this.handleChange} />;
+                inputField = <InputField type="text" object={this.props.object} field={this.props.field}
+                                         onChange={this.props.onChange} />;
                 break;
             case fieldTypes.number:
-                inputField = <Input type="number" value={this.props.object[this.props.field].toString()} onChange={this.handleChange} />;
+                inputField = <InputField type="number" object={this.props.object} field={this.props.field}
+                                         onChange={this.props.onChange} parser={parseInt} />;
                 break;
             case fieldTypes.troop:
-                inputField = (
-                    <Select value={this.props.object[this.props.field].id.toString()}
-                            onChange={this.handleChange}>
-                        {this.props.objectList ? (
-                            this.props.objectList.map((troop) => (
-                                <Option value={troop.id.toString()} key={troop.id.toString()}>
-                                    {troop.id + ' - ' + troop.name}
-                                </Option>
-                            ))
-                        ) : null}
-                    </Select>
-                );
-                break;
-            case fieldTypes.item:
-                inputField = (
-                    <Select showSearch showArrow={false} filterOption={false} value={this.props.object[this.props.field].id.toString()}
-                            onSelect={this.handleChange} onSearch={this.onItemSearch} style={{width: '100%'}} notFoundContent="No Match">
-                        {this.state.filteredItems ? (
-                            this.state.filteredItems.map((item) => (
-                                <Option value={item.id.toString()} key={item.id.toString()}>
-                                    {item.id + ' - ' + item.name}
-                                </Option>
-                            ))
-                        ) : (
-                            <Option value={this.props.object[this.props.field].id.toString()}>
-                                {this.props.object[this.props.field].id + ' - ' + this.props.object[this.props.field].name}
-                            </Option>
-                        )}
-                    </Select>
-                );
-                break;
+            case fieldTypes.faction:
             case fieldTypes.doorKey:
-                inputField = (
-                    <Select showSearch showArrow={false} filterOption={false} value={this.props.object[this.props.field].id.toString()}
-                            onSelect={this.handleChange} onSearch={this.onDoorSearch} style={{width: '100%'}} notFoundContent="No Match">
-                        {this.state.filteredDoors ? (
-                            this.state.filteredDoors.map((door) => (
-                                <Option value={door.id.toString()} key={door.id.toString()}>
-                                    {door.id + ' - ' + door.name}
-                                </Option>
-                            ))
-                        ) : (
-                            <Option value={this.props.object[this.props.field].id.toString()}>
-                                {this.props.object[this.props.field].id + ' - ' + this.props.object[this.props.field].name}
-                            </Option>
-                        )}
-                    </Select>
-                );
-                break;
             case fieldTypes.boardAccess:
-                inputField = (
-                    <Select showSearch showArrow={false} filterOption={false} value={this.props.object[this.props.field].id.toString()}
-                            onSelect={this.handleChange} onSearch={this.onBoardSearch} style={{width: '100%'}} notFoundContent="No Match">
-                        {this.state.filteredBoards ? (
-                            this.state.filteredBoards.map((board) => (
-                                <Option value={board.id.toString()} key={board.id.toString()}>
-                                    {board.id + ' - ' + board.name}
-                                </Option>
-                            ))
-                        ) : (
-                            <Option value={this.props.object[this.props.field].id.toString()}>
-                                {this.props.object[this.props.field].id + ' - ' + this.props.object[this.props.field].name}
-                            </Option>
-                        )}
-                    </Select>
-                );
+            case fieldTypes.item:
+                inputField = <SearchSelectField object={this.props.object} field={this.props.field}
+                                                objectList={this.props.objectList} onChange={this.props.onChange} />;
+                break;
         }
 
         return (
@@ -166,6 +49,69 @@ export default class PlayerField extends React.Component {
                 </Col>
                 <Col span={20}>{inputField}</Col>
             </Row>
+        );
+    }
+}
+
+class InputField extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {filteredObjects: null};
+    }
+
+    handleChange = (event) => {
+        let object = Object.assign({}, this.props.object);
+        object[this.props.field] = this.props.parser ? this.props.parser(event.target.value) : event.target.value;
+        this.props.onChange(object);
+    };
+
+    render() {
+        return <Input type={this.props.type} value={this.props.object[this.props.field].toString()} onChange={this.handleChange} />;
+    }
+}
+
+class SearchSelectField extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {filteredObjects: null};
+    }
+
+    handleChange = (event) => {
+        let object = Object.assign({}, this.props.object);
+        let selectedObject = this.props.objectList.filter((obj) => obj.id.toString() === event.toString())[0];
+        object[this.props.field] = selectedObject;
+        this.props.onChange(object);
+    };
+
+    onSearch = (searchTerm) => {
+        if (this.props.objectList && searchTerm && searchTerm !== '') {
+            let filteredObjects = this.props.objectList.filter(item =>
+                item.id.toString().includes(searchTerm) || item.name.toLowerCase().includes(searchTerm.toLowerCase())
+            );
+            this.setState({filteredObjects: filteredObjects});
+        } else {
+            this.setState({filteredObjects: null});
+        }
+    };
+
+    render() {
+        return (
+            <Select showSearch showArrow={false} filterOption={false} value={this.props.object[this.props.field].id.toString()}
+                    onSelect={this.handleChange} onSearch={this.onSearch} style={{width: '100%'}} notFoundContent="No Match">
+                {this.state.filteredObjects ? (
+                    this.state.filteredObjects.map((object) => (
+                        <Option value={object.id.toString()} key={object.id.toString()}>
+                            {object.id + ' - ' + object.name}
+                        </Option>
+                    ))
+                ) : (
+                    <Option value={this.props.object[this.props.field].id.toString()}>
+                        {this.props.object[this.props.field].id + ' - ' + this.props.object[this.props.field].name}
+                    </Option>
+                )}
+            </Select>
         );
     }
 }
